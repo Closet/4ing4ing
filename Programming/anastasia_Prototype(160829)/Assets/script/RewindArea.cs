2c3defaa9e0527fefd79b441f49c9fd053e95e8e
uPrin <503109@naver.com>
2016-08-28 23:26:46 +0900

    changeLog_160829
    깃에서 동기화해도 미싱프리팹이 안뜨도록 설정

---
+++ Programming/anastasia_Prototype(160829)/Assets/script/RewindArea.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Chronos.Example{
+	public class RewindArea : MonoBehaviour {
+
+		public bool rewindSw=false;
+		private int count = 0;
+
+		void Update()
+		{
+			if (rewindSw == true) {
+				++count;
+				if (count == 1) {
+					rewindSw = false;
+					count = 0;
+				}
+			}
+		}
+
+		void OnTriggerStay(Collider coll)
+		{
+            Debug.Log(coll.gameObject.name);
+            if (rewindSw == true && coll.gameObject.tag == "Enemy") {
+				coll.gameObject.GetComponent<TestEnemy> ().StartRewind ();
+                
+            }
+            if(rewindSw == true && coll.gameObject.tag == "Player")
+            {
+                coll.gameObject.GetComponent<testmove>().StartRewind();
+            }
+            if (rewindSw == true && coll.gameObject.tag == "anata")
+            {
+                coll.gameObject.GetComponent<anatest>().StartRewind();
+            }
+            if (rewindSw == true && coll.gameObject.tag == "something")
+            {
+                coll.gameObject.GetComponent<objectrewind>().StartRewind();
+            }
+        }
+	}	
+}
---
+++ Programming/anastasia_Prototype(160829)/Assets/script/anatest.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Chronos.Example
+{
+    public class anatest : MonoBehaviour
+    {
+        public int whoAreYou;
+        public GameObject Player1;
+        public GameObject Player2;
+        public CharacterController CC;
+        public Animation Any;
+        public int rewindCount = -1;
+        public Vector3 Vec;
+        Vector3 v1;
+        Vector3 v2;
+        public NavMeshAgent nav;
+        Quaternion dir;
+
+        public int speed = 6;
+        float TurnSpeed = 5f;
+        float Distancedir;
+
+        public bool MoveOn;
+        public bool AttackOn;
+        public bool autocome;
+        public bool runflag;
+        public bool donotcome;
+        private GlobalClock[] globalclocks;
+        private Vector3 dest;
+        private GlobalClock root;
+        private bool rewindSw;
+        private int count = 0;
+        private bool flag = false;
+        private Timeline timeline;
+        public GameObject timekeeper;
+        public bool runflag2 = false;
+       
+        // Use this for initialization
+        void Start()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            donotcome = false;
+            globalclocks = timekeeper.GetComponents<GlobalClock>();
+            for (int i = 0; i < globalclocks.Length; ++i)
+            {
+                if (globalclocks[i].key == "Root")
+                {
+                    root = globalclocks[i];
+                }
+            }
+            runflag = false;
+            autocome = false;
+            MoveOn = false;
+            timeline = GetComponent<Timeline>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rewindCount >= 0)
+            {
+                ++rewindCount;
+                if (rewindCount == 120)
+                {
+                    timeline.globalClockKey = "Character";
+                    rewindCount = -1;
+                }
+            }
+            if(donotcome==true)
+            {
+                MoveOn = false;
+                donotcome = false;
+                whoAreYou = 0;
+                nav.destination = transform.position;
+            }
+
+            if(MoveOn)
+            {
+               
+                if(whoAreYou==1)
+                {
+                    
+                    nav.destination = Player1.transform.position;
+                }
+                else if(whoAreYou==2)
+                {
+                    nav.destination = Player2.transform.position;
+                }
+                v1 = (Vec - transform.position).normalized;
+                Distancedir = Vector3.Distance(Vec, transform.position);
+                if (runflag2 == true)
+                {
+                    Debug.Log("runrunrun2");
+                    nav.speed = 5*1.2f;
+                    
+                    runflag2 = false;
+                }
+                else if (runflag == false)
+                {
+                    nav.speed = 5;
+                    CC.Move(new Vector3(0, -0.5f, 0));
+                }
+
+                else if (runflag == true)
+                {
+                    Debug.Log("runrunrunrun");
+                    nav.speed = 5 * 1.2f;
+                }
+                /*
+                if (speed != 0)
+                    dir = Quaternion.LookRotation(v1);
+                v2.y = dir.eulerAngles.y;
+                dir.eulerAngles = v2;
+
+                transform.rotation = Quaternion.Slerp(transform.rotation,dir, TurnSpeed * Time.deltaTime);*/
+            }
+                //}
+        }
+        public void StartRewind()
+        {
+            Debug.Log(timeline.globalClockKey);
+            if (rewindCount == -1)
+            {
+                timeline.globalClockKey = "Rewind";
+                rewindCount = 0;
+            }
+        }
+    }
+}
