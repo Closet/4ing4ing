137aa3699818abbe7288967ae6f0c298510878db
uPrin <uPrin@Hyowonui-MacBook.local>
2016-09-03 14:29:54 +0900

    changLog_160903
    적이 문여는 스크립트 구현중

---
+++ Programming/Anastasia_Prototype(beta)/Assets/script/Jail_Protoypte_withEnemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Jail_Protoypte_withEnemy : MonoBehaviour {
+
+	public Animator doorani;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider Get)
+	{
+		if ((Get.GetComponent<Collider>().tag == "Player" && Variable.char_flag == false) && Input.GetKeyDown("g"))
+		{
+			Debug.Log("충돌함");
+			doorani.SetTrigger("door_Trigger");
+			//   UnityEditor.NavMeshBuilder.BuildNavMesh();
+		}
+		if ((Get.GetComponent<Collider>().tag == "Player2" && Variable.char_flag == true) && Input.GetKeyDown("g"))
+		{
+			Debug.Log("충돌함");
+			doorani.SetTrigger("door_Trigger");
+			//   UnityEditor.NavMeshBuilder.BuildNavMesh();
+		}
+		/*
+		if (Get.GetComponent<Collider> ().tag == "Enemy") {
+			{
+				if (Get.GetComponent<TestEnemy> ().Mode == 4)
+					Debug.Log ("enemy충돌함");
+				doorani.SetTrigger ("door_Trigger");
+			}
+		}
+		*/
+
+
+	}
+}
